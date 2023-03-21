4338ce58daf4d27a57a5148cce8bf63e6fd3ae1c
Bryan Parada <bryan.parada.cea@gmail.com>
2023-03-21 13:49:13 -0300

    FA280 - Home Screen design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+import 'package:stripe_app/data/cards.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pay'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add), 
+            onPressed: null,
+            ),
+        ]
       ),
-      body: Center(
-        child: Text('homePage'),
+      body:Stack(
+        children: [
+
+          Positioned(
+            width: size.width,
+            height: size.height,
+            top: 200,
+            child: PageView.builder(
+              controller: PageController(
+                viewportFraction: 0.9
+              ),
+              physics: BouncingScrollPhysics(),
+              itemCount: cards.length,
+              itemBuilder: (_, i) {
+          
+                final card = cards[i];
+          
+                return CreditCardWidget(
+                  cardNumber: card.cardNumberHidden,
+                  expiryDate: card.expiracyDate,
+                  cardHolderName: card.cardHolderName,
+                  isHolderNameVisible: true,
+                  cvvCode: card.cvv, 
+                  showBackView: false, 
+                  onCreditCardWidgetChange: (creditCardBrand) {}
+                  );
+          
+              }
+              ),
+          )
+
+
+        ],
       ),
     );
   }
