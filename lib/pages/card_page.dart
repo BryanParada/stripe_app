5c92a2f7f0a7d0940dadc9fe51d182fd3a0cad6b
Bryan Parada <bryan.parada.cea@gmail.com>
2023-03-21 15:21:52 -0300

    FA282 - Card Page - selected card - Hero anim

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+import 'package:stripe_app/models/credit_card.dart';
+
+import '../widgets/total_pay_button.dart';
 
 class CardPage extends StatelessWidget {
   const CardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+  final card = CreditCard(
+        cardNumberHidden: '4242',
+        cardNumber: '4242424242424242',
+        brand: 'visa',
+        cvv: '213',
+        expiracyDate: '01/25',
+        cardHolderName: 'Juan Perez'
+      );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pay'),
+      ),
+      body: Stack(
+        children: [
+
+          Container(),
+
+          Hero(
+            tag: card.cardNumber,
+            child: CreditCardWidget(
+                      cardNumber: card.cardNumberHidden,
+                      expiryDate: card.expiracyDate,
+                      cardHolderName: card.cardHolderName,
+                      isHolderNameVisible: true,
+                      cvvCode: card.cvv, 
+                      showBackView: false, 
+                      onCreditCardWidgetChange: (creditCardBrand) {}
+                      ),
+          ),
+
+             Positioned(
+            bottom: 0,
+            child: TotalPayButton()
+          ),
+
+
+
+        ],
+      )
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/credit_card_widget.dart';
 import 'package:stripe_app/data/cards.dart';
+import 'package:stripe_app/helpers/helpers.dart';
+import 'package:stripe_app/pages/card_page.dart';
 import 'package:stripe_app/widgets/total_pay_button.dart';
 
 class HomePage extends StatelessWidget {
@@ -38,15 +40,24 @@ class HomePage extends StatelessWidget {
           
                 final card = cards[i];
           
-                return CreditCardWidget(
-                  cardNumber: card.cardNumberHidden,
-                  expiryDate: card.expiracyDate,
-                  cardHolderName: card.cardHolderName,
-                  isHolderNameVisible: true,
-                  cvvCode: card.cvv, 
-                  showBackView: false, 
-                  onCreditCardWidgetChange: (creditCardBrand) {}
-                  );
+                return GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, navigateFadeIn(context, CardPage()));
+                    
+                  },
+                  child: Hero(
+                    tag: card.cardNumber,
+                    child: CreditCardWidget(
+                      cardNumber: card.cardNumberHidden,
+                      expiryDate: card.expiracyDate,
+                      cardHolderName: card.cardHolderName,
+                      isHolderNameVisible: true,
+                      cvvCode: card.cvv, 
+                      showBackView: false, 
+                      onCreditCardWidgetChange: (creditCardBrand) {}
+                      ),
+                  ),
+                );
           
               }
               ),
