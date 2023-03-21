8dea72420c084fcf429c7b13c28eacd880d72dc5
Bryan Parada <bryan.parada.cea@gmail.com>
2023-03-21 14:19:14 -0300

    FA281 - Pay amount - pay button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/credit_card_widget.dart';
 import 'package:stripe_app/data/cards.dart';
+import 'package:stripe_app/widgets/total_pay_button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -49,7 +50,13 @@ class HomePage extends StatelessWidget {
           
               }
               ),
-          )
+          ),
+
+
+          Positioned(
+            bottom: 0,
+            child: TotalPayButton()
+          ),
 
 
         ],
